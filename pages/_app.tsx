04e7306adf85c5c03ec9e073330d2fdc019aa645
Raveesh Bhalla <raveeshbhalla90@gmail.com>
2023-04-01 14:09:48 -0700

    added nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,10 @@ import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react';
 import { createTheme, NextUIProvider } from "@nextui-org/react"
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+import Router from 'next/router'
+import { useEffect } from 'react';
 
 // 2. Call `createTheme` and pass your custom values
 const lightTheme = createTheme({
@@ -19,6 +23,16 @@ const darkTheme = createTheme({
 })
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    Router.events.on('routeChangeStart', NProgress.start)
+    Router.events.on('routeChangeComplete', NProgress.done)
+    Router.events.on('routeChangeError', NProgress.done)
+    return () => {
+      Router.events.off('routeChangeStart', NProgress.start)
+      Router.events.off('routeChangeComplete', NProgress.done)
+      Router.events.off('routeChangeError', NProgress.done)
+    }
+  }, [])
   return <NextThemesProvider
             defaultTheme="system"
             attribute="class"
